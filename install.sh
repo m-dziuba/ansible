ec514bd73d15d7301b1d1e47cf6bba62a2263b20
Mateusz Dziuba <mateusz.dziuba97@gmail.command>
2024-01-10 23:18:50 +0100

    Fix install.sh and core-setup

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo pacman -Sy ansible --noconfirm --needed
 ansible-playbook local.yml -J -K
-git remote set-url git@github.com:m-dziuba/ansible
+git remote set-url origin git@github.com:m-dziuba/ansible
